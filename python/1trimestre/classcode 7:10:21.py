b3a99671c7cc8fb22bcd85db37c37a7eb06d7ce7
camilaes6 <104382001+camilaes6@users.noreply.github.com>
2022-05-31 11:22:58 -0500

    Add files via upload

---
+++ python/1trimestre/classcode 7:10:21.py
@@ -0,0 +1,15 @@
+print("reprobados")
+print("5")
+
+
+mensaje="aprobado"
+mensaje2="reprobados"
+
+print(mensaje)
+print(mensaje2)
+
+mensaje="mas tarea para el 9 virtual"
+
+print(mensaje)
+
+print("este es el mensaje guardado en memoria: ", mensaje)
