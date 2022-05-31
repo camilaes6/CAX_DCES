2f591092887729edf588e477325c10d039818248
camilaes6 <104382001+camilaes6@users.noreply.github.com>
2022-05-31 11:44:33 -0500

    Add files via upload

---
+++ python/1trimestre/21:10:21.py
@@ -0,0 +1,11 @@
+print("how can we act with empathy and positivity when were online?")
+
+print("we shouldnt make fun of anyone, take care of what we post, we should take care of who we make famous, if someone did us wrong we shouldnt want to take revenge by postimg something about them in any social media")
+
+print("identify: a boy who gave a letter to a gril, this girl sent the letter to her friend who posted it online and evryone started to make fun of him")
+
+print("feel: i think the guy felt disappointed of the girl because he was really into her, the gril amd her friend probably felt guilty and bad because they didnt want that to happen")
+
+print("imagine: if the girl didnt sent that to her friend and kept the letter to herself she could answer to the guy honestly and maybe correspond him")
+
+print("say: i think that if this situation was presented to the characters before all this happened the girl wouldve thought twice about semding the letter to her friend and the friend propably shouldnt have posted it")
---
+++ python/1trimestre/code for the week 14:10:21.py
@@ -0,0 +1,51 @@
+#code for the week
+
+mate=8.2
+mate2=7.5
+mate3=9.8
+
+suma1=mate+mate2+mate3
+prom_mate=suma1/3
+print("el promedio de mate es: ", prom_mate)
+
+ict=9.1
+ict2=9.6
+ict3=10
+
+suma2=ict+ict2+ict3
+prom_ict=suma2/3
+print("el promedio de ict es: ", prom_ict)
+
+la=9.4
+la2=9.9
+la3=9.6
+
+suma3=la+la2+la3
+prom_la=suma3/3
+print("el promedio de language es: ", prom_la)
+
+ss=8.3
+ss2=9.8
+ss3=9.7
+
+suma4=ss+ss2+ss3
+prom_ss=suma4/3
+print("el promedio de social es: ", prom_ss)
+
+mun=8.8
+mun2=9.4
+mun3=9.3
+
+suma5=mun+mun2+mun3
+prom_mun=suma5/3
+print("el promedio de mun es: ", prom_mun)
+
+mate=8.5
+ict=9.5
+la=9.6
+ss=9.2
+mun=9.1
+
+suma6=mate+ict+la+ss+mun
+prom_final=suma6/5
+print("mi promedio final es de: ", prom_final)
