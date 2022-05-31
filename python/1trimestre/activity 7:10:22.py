9823ca073d8eea6c76b280f87e04c1bbf04bcc23
camilaes6 <104382001+camilaes6@users.noreply.github.com>
2022-05-31 11:41:48 -0500

    Add files via upload

---
+++ python/1trimestre/activity 7:10:22.py
@@ -0,0 +1,23 @@
+name="Camila"
+print("my name is", name)
+
+gradegruop="9D"
+print("i am in", gradegruop)
+
+food="carpaccio"
+print("my favorite food is", food)
+
+random="i'm happy tomorrow is friday"
+print("i want to tell the mr that", random)
+
+name="Sam"
+print("my name is", name)
+
+gradegroup="9D"
+print("i am in", gradegroup)
+
+food="pasta"
+print("my favorite food is", food)
+
+random= "i like watching netflix"
+print("i want to tell the mr that", random)
---
+++ python/1trimestre/classcode 14:10:22.py
@@ -0,0 +1,33 @@
+trombones76="hola"
+more=100000
+classe="hello there"
+
+#math operators
+
+alpha=5
+b3t4=7
+
+suma=alpha+b3t4
+print("la suma fue: ", suma)
+
+resta=alpha-b3t4
+print("la resta fue: ", resta)
+
+multp=alpha*b3t4
+print("producto: ", multp)
+
+div=alpha/b3t4
+print("resultado: ", div)
+
+pot=2**5
+print("potencia: ", pot)
+
+#code for the week:
+
+mct=8
+mct2=9
+mct3=5
+
+suma=mct+mct2+mct3
+prom_mct=suma/3
+print("el promedio de mct es: ", prom_mct)
