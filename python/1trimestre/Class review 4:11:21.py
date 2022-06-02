72b50e1c3a66f57182df6d0b900cee9735491577
camilaes6 <104382001+camilaes6@users.noreply.github.com>
2022-06-02 08:29:07 -0500

    Add files via upload

---
+++ python/1trimestre/Class review 4:11:21.py
@@ -0,0 +1,19 @@
+greeting="helloo"
+print(greeting)
+
+print("enter your greeting")
+greeting=input()
+print("you wrote ", greeting)
+
+print("write your name ")
+name=input()
+print("your name is: ", name)
+
+group=input("in which group and grade you are: ")
+print("your group and grade are ",group)
+
+food=input("what is your favorite food: ")
+print("your favorite food is ", food)
+
+random=input("something you want to tell mr roberto: ")
+print("you told mr roberto ", random)
---
+++ python/1trimestre/Code for the week 4:11:21.py
@@ -0,0 +1,26 @@
+greeting=input("say hi: ")
+print("you said ", greeting)
+
+name=input("whats your name: ")
+print("your name is ", name)
+
+live=input("where are you from: ")
+print("you are from ", live)
+
+color=input("whats your favorite color: ")
+print("your favorite color is ", color)
+
+age=input("how old are you: ")
+print("your age is ", age)
+
+animal=input("whats your favorite animal: ")
+print("your favorite animal is ", animal)
+
+hobby=input("whats your hobby: ")
+print("your hobby is ", hobby)
+
+food=input("whats your favorite food: ")
+print("your favorite food is ", food)
+
+game=input("whats your favorite game: ")
+print("your favorite game is ", game)
